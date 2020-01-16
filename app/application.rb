f6f0eb9685ba3fac12a02a207738fccc6e078f2d
Anthony Tilelli <anthony@tilelli.me>
2020-01-16 01:34:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "Your cart is empty"
       else
         @@cart.each do |item|
-          resp.write "#{item}\n"
+          resp.write item.to_s + "\n"
         end
       end
     else
